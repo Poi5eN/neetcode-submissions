497510b7caf3aea4b513cb0ec6302796f85365c7
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2026-08-03 00:58:16 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let xor = 0
+
+        for(let i=0; i<nums.length; i++){
+            xor = xor ^ nums[i]
+        }
+
+        return xor
+    }
+}
